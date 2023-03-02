7f0353045335616de5e32c2ec045b558579faaca
khoane03 <khoatk4444@gmail.com>
2023-03-03 01:15:32 +0700

    Finnal_update

---
+++ src/OOP_T2/Bai1.java
@@ -0,0 +1,22 @@
+package OOP_T2;
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner Enter = new Scanner(System.in);
+        System.out.print("Nhap so thu nhat : ");
+        int a = Enter.nextInt();
+        System.out.print("Nhap so thu hai : ");
+        int b = Enter.nextInt();
+        System.out.println("Tong : " + (a + b));
+        System.out.println("Hieu : " + (a - b));
+        System.out.println("Tich : " + (a * b));
+        System.out.println("Thuong : " + (a /(1.0*b)));
+        System.out.println("Chia lay phan du : " + (a % b));
+        int temp = (a>b)?a:b;
+        System.out.println("So "  + temp + " la so lon hon ");
+
+
+
+    }
+}
