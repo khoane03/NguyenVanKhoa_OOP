89a66964c17df95a3d1cd58ec027acc45b24b635
khoane03 <khoatk4444@gmail.com>
2023-03-03 01:15:50 +0700

    Finnal_update

---
+++ src/OOP_T1/NguyenVanKhoa_App.java
@@ -0,0 +1,9 @@
+public class NguyenVanKhoa_App {
+    public static void main(String[] args) {
+     System.out.println("Nguyen Van Khoa Hoc Java");
+     //tesst
+     
+     System.out.print("2121050782");
+    }
+    
+ }
